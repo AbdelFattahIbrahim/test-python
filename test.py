2d7044c704ed65568f28f05e4f0191099c013a39
Zulficardude <101786457+Zulficardude@users.noreply.github.com>
2022-06-15 20:41:00 +0400

    Add file via upload
    # First upload of my python code
    Testing README.md editing

---
+++ test.py
@@ -0,0 +1,27 @@
+print("Welcome to my python program! WOW")
+
+#_age var
+_age = 35
+#_name var
+_name = "Abdel Fattah Ibrahim"
+#_isCool var
+_isCool = True
+
+
+#print your age
+def printAge():
+ print(_age)
+printAge() 
+#print your name
+def printName():
+ print(_name)
+printName()
+#print true/false is you are cool or not
+def printCoolStatus():
+ #print
+ #if cool, then print "yes i am cool"
+ #if not cool, print, "totally not cool"
+ if _age > 34:
+  print("yes i am cool")
+printCoolStatus()
+  
